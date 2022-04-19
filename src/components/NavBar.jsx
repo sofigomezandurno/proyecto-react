cc83111e25a4dea7e0abeb7c2796c1abd9938b9f
sofia gomez andurno <sofigomezandurno@gmail.com>
2022-04-19 18:04:15 -0300

    NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,36 @@
-import React from "react" ;
+import React from "react";
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button, Container } from 'react-bootstrap';
 
-export default function NavBar() {
+
+const CustomNavBar = () => {
     return (
-        <Navbar bg="light" expand="lg">
-    <Container fluid>
-        <Navbar.Brand href="#">Cosmética Cba</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-            <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+        <Navbar>
+            <Container fluid>
+                <Navbar.Brand href="#">Cosmética Cba</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
 
-        <Nav.Link href="#action1">Inicio</Nav.Link>
-        <Nav.Link href="#action2">Nosotros</Nav.Link>
-        <NavDropdown title="Productos" id="navbarScrollingDropdown">
-        <NavDropdown.Item href="#action3">Cosméticos</NavDropdown.Item>
-        <NavDropdown.Item href="#action4">Accesorios</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action5">
-            Creá tu rutina ideal
-        </NavDropdown.Item>
-        </NavDropdown>
-        <Nav.Link href="#action6"> Carrito</Nav.Link>
-        </Nav>
-    <Form className="d-flex">
-        <FormControl type="search" placeholder="Qué estas buscando?" className="me-2" aria-label="Search" />
-            <Button variant="outline-success">Buscar</Button>
-        </Form>
-    </Navbar.Collapse>
-    </Container>
-</Navbar>
+                        <Nav.Link href="#action1">Inicio</Nav.Link>
+                        <Nav.Link href="#action2">Nosotros</Nav.Link>
+                        <NavDropdown title="Productos" id="navbarScrollingDropdown">
+                            <NavDropdown.Item href="#action3">Cosméticos</NavDropdown.Item>
+                            <NavDropdown.Item href="#action4">Accesorios</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action5">
+                                Creá tu rutina ideal
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#action6"> Carrito</Nav.Link>
+                    </Nav>
+                    <Form className="d-flex">
+                        <FormControl type="search" placeholder="Qué estas buscando?" className="me-2" aria-label="Search" />
+                        <Button variant="outline-success">Buscar</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
-}
+};
+
+export default CustomNavBar;
